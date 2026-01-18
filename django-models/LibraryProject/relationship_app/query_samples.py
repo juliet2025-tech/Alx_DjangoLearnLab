557473cf485aa6d10a9ac248a7ec43131259675c
ojide chibundo <chibundojuliet21@gmail.com>
2026-01-18 22:09:50 +0100

    dgango

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,28 +1,29 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-
 # Query all books by a specific author
 def get_books_by_author(author_name):
-    """
-    Returns all books written by a given author
-    """
-    author = Author.objects.get(name=author_name)
-    return author.books.all()
-
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)  # <- must use .filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return []
 
 # List all books in a library
 def get_books_in_library(library_name):
-    """
-    Returns all books available in a given library
-    """
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
-
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        return books
+    except Library.DoesNotExist:
+        return []
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    """
-    Returns the librarian managing a given library
-    """
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian
+        return librarian
+    except Library.DoesNotExist:
+        return None
+
