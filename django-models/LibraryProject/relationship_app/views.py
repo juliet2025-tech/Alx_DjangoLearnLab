c803a6d86f123d908048a9e30c3e2ae5cdb177b3
ojide chibundo <chibundojuliet21@gmail.com>
2026-01-22 17:28:15 +0100

    views update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -49,6 +49,9 @@ from django.contrib.auth.decorators import user_passes_test
 def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
+["from django.views.generic.detail import DetailView"]
+
+
 # ===== ADMIN VIEW =====
 @user_passes_test(is_admin)
 def admin_view(request):
