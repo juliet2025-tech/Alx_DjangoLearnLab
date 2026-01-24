9715085c044b1da276c80b4233ac339882f4e9ff
ojide chibundo <chibundojuliet21@gmail.com>
2026-01-24 16:49:35 +0100

    advanced features

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from .forms import ExampleForm
+
 from django.contrib.auth.decorators import permission_required
 from .models import Book  # or whatever your book model is
 
@@ -10,3 +12,12 @@ def book_list(request):
 
 
 # Create your views here.
+from django.shortcuts import render
+from .forms import ExampleForm
+
+def form_example(request):
+    """
+    Demonstrates secure form handling with CSRF protection and input validation.
+    """
+    form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
