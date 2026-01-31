4852a49a834fc6bc6500ada31a7d388445f31019
ojide chibundo <chibundojuliet21@gmail.com>
2026-01-31 13:00:35 +0100

    djangolearnlab

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 
+# Existing ListAPIView (keep this)
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+# New ViewSet for full CRUD
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
