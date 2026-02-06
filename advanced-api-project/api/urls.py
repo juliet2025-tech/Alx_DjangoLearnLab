99111e78a345de4bc324d1409f724d8e351e4402
ojide chibundo <chibundojuliet21@gmail.com>
2026-02-06 22:20:29 +0100

    advanced

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,9 +8,19 @@ from .views import (
 )
 
 urlpatterns = [
+    # Read operations
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+
+    # Write operations
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
