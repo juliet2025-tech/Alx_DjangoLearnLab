4c65da99ba08d889d88cd13deddc990cd3f8ddcc
ojide chibundo <chibundojuliet21@gmail.com>
2026-02-08 10:50:55 +0100

    advan

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,13 +3,32 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # List all books (read-only for unauthenticated users)
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Enable filtering, searching, and ordering
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
+    ]
+
+    # Filtering
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Searching
+    search_fields = ['title', 'author']
+
+    # Ordering
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
+
 
 
 # Retrieve a single book (read-only for unauthenticated users)
@@ -38,6 +57,10 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+    
+
+
+
 
   # Only logged-in users
 
