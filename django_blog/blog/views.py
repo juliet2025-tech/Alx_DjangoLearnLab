217356107c2a3cc58f26da1ff4fb73348d7d756b
ojide chibundo <chibundojuliet21@gmail.com>
2026-02-15 22:57:56 +0100

    learnLab

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, ProfileUpdateForm
+
+# REGISTER VIEW
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = RegisterForm()
+
+    return render(request, "blog/register.html", {"form": form})
+
+
+# PROFILE VIEW
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, "blog/profile.html", {"form": form})
 
 # Create your views here.
